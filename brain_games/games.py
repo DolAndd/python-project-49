e94e9b0efbdc6e75a1762aaefe77da95371067f6
Andrey Dolganov <dolganov2002@mail.ru>
2024-07-26 21:22:35 +0300

    update brain-even

--- brain_games/games.py
+++ brain_games/games.py
@@ -6,22 +6,23 @@ def guess_even_number():
     player_name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     true_answer_count = 0
-    while true_answer_count < 3:
+    for i in range(3):
         random_number = randint(1, 100)
         print('Question:', random_number)
-        print('Your answer: ', end='')
+        print('Your answer: ', end ='')
         answer = input()
-
-        if (random_number % 2 == 0 and answer == 'yes') or \
-           (random_number % 2 == 1 and answer == 'no'):
+        right_answer = ''
+        if random_number % 2 == 0:
+            right_answer = 'yes'
+        else:
+            right_answer = 'no'
+        if right_answer == answer:
             print('Correct!')
             true_answer_count += 1
-        elif random_number % 2 == 0 and answer != 'yes':
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
-            print(f"Let's try again, {player_name}")
-            true_answer_count = 0
-        elif random_number % 2 == 1 and answer != 'no':
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
-            print(f"Let's try again, {player_name}")
-            true_answer_count = 0
-    print(f'Congratulations, {player_name}')
+        else:
+            print(f"'{answer}' is wrong answer ;(. \
+Correct answer was '{right_answer}'.")
+            print("Let's try again, Bill!")
+            break
+    else:
+        print('Congratulations, Bill!')
