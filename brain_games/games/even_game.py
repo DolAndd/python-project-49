c8430c495987e4f61a55cb9ce8a87428cf94d453
Andrey Dolganov <dolganov2002@mail.ru>
2024-08-08 22:55:56 +0300

    update even and prime_game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -3,11 +3,10 @@ EXERCISE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(num):
-    return 'yes' if num % 2 == 0 else 'no'
+    return num % 2 == 0
 
 
 def get_question_and_right_answer():
-    random_number = randint(1, 100)
-    condition = str(random_number)
-    right_answer = is_even(random_number)
-    return condition, str(right_answer)
+    condition = randint(1, 100)
+    right_answer = 'yes' if is_even(condition) else 'no'
+    return str(condition), str(right_answer)
--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -5,12 +5,11 @@ EXERCISE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(num):
     for i in range(2, num // 2 + 1):
         if num % i == 0:
-            return 'no'
-    return 'yes'
+            return False
+    return True
 
 
 def get_question_and_right_answer():
-    random_number = randint(2, 100)
-    condition = str(random_number)
-    right_answer = is_prime(random_number)
-    return condition, str(right_answer)
+    condition = randint(2, 100)
+    right_answer = 'yes' if is_prime(condition) else 'no'
+    return str(condition), str(right_answer)
